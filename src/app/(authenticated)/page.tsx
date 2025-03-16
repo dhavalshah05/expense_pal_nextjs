bce70c325421a77cb542eed99e6c233e61eb274d
Dhaval Shah <dhaval@alphastack.in>
2025-03-16 22:32:17 +0530

    Create layout groups for adding header in authenticated routes

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,9 +1,6 @@
-import Header from "@/features/shared/components/header/Header";
-
 export default function Home() {
     return (
         <div>
-            <Header />
             <h2>Home Page</h2>
         </div>
     );
