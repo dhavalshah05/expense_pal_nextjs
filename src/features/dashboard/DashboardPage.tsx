ef81fe8ffe264c52bec4d9675961c488bdfe782b
Dhaval Shah <dhaval@alphastack.in>
2025-03-21 17:51:33 +0530

    Summary by category card

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import {categoriesPageRoute, expensesPageRoute} from "@/utils/routing/route-names";
 import currencyUtils from "@/utils/currency/currency-utils";
 import {db} from "@/db";
-import {eq, sum} from "drizzle-orm";
-import {expensesTable} from "@/db/schema";
+import {desc, eq, sql, sum} from "drizzle-orm";
+import {categoriesTable, expensesTable} from "@/db/schema";
 import getUserIdFromHeader from "@/features/shared/hooks/get-user-id-from-header";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 
 export default async function DashboardPage() {
     const userId = await getUserIdFromHeader();
@@ -18,7 +19,10 @@ export default async function DashboardPage() {
                 <Link href={categoriesPageRoute()}>Categories</Link>
             </p>
 
-            <TotalExpenseCard userId={userId} />
+            <section className={"flex flex-col gap-4"}>
+                <TotalExpenseCard userId={userId} />
+                <SummaryByCategoryCard userId={userId} />
+            </section>
         </div>
     );
 }
@@ -43,6 +47,55 @@ async function TotalExpenseCard(
         <section className={"bg-gray-200 p-4 rounded-lg"}>
             <h3 className={"text-sm text-gray-700"}>Total Expense (March)</h3>
             <p className={"text-2xl text-gray-900 font-bold mt-2"}>{total}</p>
+        </section>
+    )
+}
+
+async function SummaryByCategoryCard(
+    {
+        userId,
+    }: {
+        userId: string
+    }
+) {
+    const results = await db.select({
+        categoryId: categoriesTable.id,
+        categoryName: categoriesTable.name,
+        amount: sum(expensesTable.amount),
+    })
+        .from(expensesTable)
+        .innerJoin(categoriesTable, eq(expensesTable.categoryId, categoriesTable.id))
+        .where(eq(expensesTable.userId, userId))
+        .orderBy(desc(sum(expensesTable.amount)))
+        .groupBy(categoriesTable.id, categoriesTable.name);
+
+    const summaryResult = results.map((item) => {
+        return {
+            id: item.categoryId,
+            name: item.categoryName,
+            amount: currencyUtils.formatCurrency(currencyUtils.fromPaisa(Number(item.amount)))
+        }
+    })
+
+    return (
+        <section className={"bg-gray-200 p-4 rounded-lg"}>
+            <h3 className={"text-sm text-gray-700"}>Summary by category</h3>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Category</TableHead>
+                        <TableHead className={"text-right"}>Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {summaryResult.map(result => (
+                        <TableRow key={result.id}>
+                            <TableCell>{result.name}</TableCell>
+                            <TableCell className={"text-right text-gray-900 font-bold"}>{result.amount}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </section>
     )
 }
